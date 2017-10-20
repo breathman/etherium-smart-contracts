7ecb4cbec8f270fe9efafb5244b97634c75f79d6
m.dykhalkin <m.dykhalkin@tinkoff.ru>
2017-10-20 18:20:08 +0300

    first token

---
+++ MyToken.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.15;
+
+contract MyToken {
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    mapping (address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function MyToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) {
+        balanceOf[msg.sender] = initialSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
+        decimals = decimalUnits;
+    }
+
+    function transfer(address _to, uint256 _value) {
+        /* Check if sender has balance and for overflows */
+        require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
+
+        /* Add and subtract new balances */
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+windows
+Для запуска локальной p2p сети необходимо:
+
+Установить Geth
+Установить Mist
+
+Запускаем Geth.
+geth  --dev --rpc --rpcapi \.\pipe\geth.ipc console
+
+Запускаем Mist.
+
+C:\Program Files\Mist\Mist.exe localhost:8545
