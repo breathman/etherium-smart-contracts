eecf6d5448a79b544525b49304e34b89a9514521
m.dykhalkin <m.dykhalkin@tinkoff.ru>
2017-11-01 18:50:55 +0300

    inheritance

--- SimpleContract.sol
+++ SimpleContract.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.4.15;
 
+contract owned {
+    address  public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
+
 contract SimpleContract is owned {
 
     string public name;
@@ -24,21 +41,4 @@ contract SimpleContract is owned {
         Transfer(msg.sender, _to, _value);
     }
 
-}
-
-contract owned {
-    address  public owner;
-
-    function owned() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        owner = newOwner;
-    }
 }
